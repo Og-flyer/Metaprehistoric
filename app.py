da5092bc024ee93314f5032ea522763e150f99da
Og-flyer <Og16kaan@hotmail.com>
2026-04-06 18:13:21 +0300

    Enhance guessing logic to prevent duplicate guesses and display previous attempts

--- app.py
+++ app.py
@@ -17,10 +17,16 @@ def game(Daily_chosen):
     Daily_chosen_DATA = Daily_chosen
     if "won" not in st.session_state:
         st.session_state.won = False
+    if "guesses" not in st.session_state:
+        st.session_state.guesses = []
     if not st.session_state.won:
         guess = st_searchbox(search_creatures, placeholder="Write your guess: ")
         if st.button("Guess!"):
             if guess is None:
+                st.stop()
+            already_guessed = [r.get("guess_name") for r in st.session_state.guesses]
+            if guess in already_guessed:
+                st.warning("You already guessed that!")
                 st.stop()
             guess_DATA = find_creature(guess)
             if guess_DATA is None:
@@ -31,21 +37,39 @@ def game(Daily_chosen):
                 st.rerun()
             else:
                 st.error("Wrong!")
+                result = {}
                 Last_Match = None
                 for i in Daily_chosen_DATA:
                     if i == "name":
                         continue
                     elif guess_DATA[i]["value"] == Daily_chosen_DATA[i]["value"]:
-                        st.write(f"{i}: {Daily_chosen_DATA[i]['value']}")
-                        Last_Match = i
-                st.markdown(Daily_chosen_DATA[Last_Match]["wiki"])
+                         result[i] = (Daily_chosen[i]["value"], Daily_chosen[i]["wiki"])
+                         Last_Match = i
+                result["guess_name"] = guess
+                result["last_match"] = Last_Match
+                st.session_state.guesses.append(result)        
     else:
+        st.session_state.guesses = []
         st.success(f"Correct! The answer was {Daily_chosen_DATA['name']}")
         for i in Daily_chosen_DATA:
                 if i == "name":
                     continue
                 st.write(f"{i}: {Daily_chosen_DATA[i]['value']}")
         st.markdown(Daily_chosen_DATA["species"]["wiki"])
+    
+    for result in st.session_state.guesses:
+        Last_Match = result.get("last_match")
+        for key, val in result.items():
+            if key == "last_match":
+                continue
+            if key == "guess_name":
+                continue
+            value, wiki = val
+            st.write(f"{key}: {value}")
+        if Last_Match:
+            st.markdown(f"[{Daily_chosen[Last_Match]['value']}]({Daily_chosen[Last_Match]['wiki']})")
+        st.divider()
+    
     return None
 
 def Practice():
