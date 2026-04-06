13d7d9876a77bd06ac0f0dc1f673057bd97a8dd9
Og-flyer <Og16kaan@hotmail.com>
2026-04-06 16:50:17 +0300

    add hide function for guessing

--- app.py
+++ app.py
@@ -14,32 +14,38 @@ def search_creatures(query):
 st.title("Metaprehistoric")
 
 def game(Daily_chosen):
-    guess = st_searchbox(search_creatures, placeholder="Write your guess: ")
-    if st.button("Guess!"):
-        if guess is None:
-            st.stop()
-        Daily_chosen_DATA = Daily_chosen
-        guess_DATA = find_creature(guess)
-        if guess_DATA is None:
-            st.error("Creature not found!")
-            st.stop()
-        if guess_DATA["name"] == Daily_chosen_DATA["name"]:
-            st.success(f"Correct! The answer was {Daily_chosen_DATA['name']}")
-            for i in Daily_chosen_DATA:
+    Daily_chosen_DATA = Daily_chosen
+    if "won" not in st.session_state:
+        st.session_state.won = False
+    if not st.session_state.won:
+        guess = st_searchbox(search_creatures, placeholder="Write your guess: ")
+        if st.button("Guess!"):
+            if guess is None:
+                st.stop()
+            guess_DATA = find_creature(guess)
+            if guess_DATA is None:
+                st.error("Creature not found!")
+                st.stop()
+            if guess_DATA["name"] == Daily_chosen_DATA["name"]:
+                st.session_state.won = True
+                st.rerun()
+            else:
+                st.error("Wrong!")
+                Last_Match = None
+                for i in Daily_chosen_DATA:
+                    if i == "name":
+                        continue
+                    elif guess_DATA[i]["value"] == Daily_chosen_DATA[i]["value"]:
+                        st.write(f"{i}: {Daily_chosen_DATA[i]['value']}")
+                        Last_Match = i
+                st.markdown(Daily_chosen_DATA[Last_Match]["wiki"])
+    else:
+        st.success(f"Correct! The answer was {Daily_chosen_DATA['name']}")
+        for i in Daily_chosen_DATA:
                 if i == "name":
                     continue
                 st.write(f"{i}: {Daily_chosen_DATA[i]['value']}")
-            st.markdown(Daily_chosen_DATA["species"]["wiki"])
-        else:
-            st.error("Wrong!")
-            Last_Match = None
-            for i in Daily_chosen_DATA:
-                if i == "name":
-                    continue
-                elif guess_DATA[i]["value"] == Daily_chosen_DATA[i]["value"]:
-                    st.write(f"{i}: {Daily_chosen_DATA[i]['value']}")
-                    Last_Match = i
-            st.markdown(Daily_chosen_DATA[Last_Match]["wiki"])
+        st.markdown(Daily_chosen_DATA["species"]["wiki"])
     return None
 
 def Practice():
